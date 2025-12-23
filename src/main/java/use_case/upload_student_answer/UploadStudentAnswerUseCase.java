eeb725baf95e681a5b398c5d48a4e1035ec360dd
Pepper256 <splotchyscar@gmail.com>
2025-12-22 21:04:04 -0800

    starter code of use case

--- src/main/java/use_case/upload_student_answer/UploadStudentAnswerUseCase.java
+++ src/main/java/use_case/upload_student_answer/UploadStudentAnswerUseCase.java
@@ -2,9 +2,18 @@ package use_case.upload_student_answer;
 
 import use_case.dto.UploadStudentAnswerInputData;
 
+import java.util.List;
+
 public class UploadStudentAnswerUseCase implements UploadStudentAnswerInputBoundary{
+
+    private final UploadStudentAnswerDataAccessInterface dao;
+
+    public UploadStudentAnswerUseCase(UploadStudentAnswerDataAccessInterface dao) {
+        this.dao = dao;
+    }
+
     @Override
     public void execute(UploadStudentAnswerInputData inputData) {
-
+        List<String> paths = inputData.getPaths();
     }
 }
